4b566b618b17775c1fec9be4d22b14102aacfed1
Patrick McCarty <patrickthesailorman@gmail.com>
2017-11-21 02:06:37 +0000

    Set display details function

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,11 @@ var hotel = {
             name: "Luxury Suite",
             price: "$250.00",
             available: 5
+        },
+        {
+            name: "Penthouse w/ pool",
+            price: "$6.00",
+            available: 2
         }
     ],
     name: "CareerDevs Innt"
@@ -22,15 +27,33 @@ var hotel = {
 // console.log(document.getElementById("radio").nodeName);
 // console.log(document.getElementById("label").nodeName);
 
+function displayDetails(room) {
+    console.log(room);
+    document.getElementById("available").innerHTML = hotel.rooms[room].available;
+    document.getElementById("price").innerHTML = hotel.rooms[room].price;
+}
+
 for(var i = 0; i < hotel.rooms.length; i++) {
+    console.log("start");
     var radioBtn = document.createElement("INPUT");
     radioBtn.setAttribute("type", "radio");
     radioBtn.setAttribute("name", "roomType");
     radioBtn.setAttribute("value", i);
     radioBtn.setAttribute("id", "room" + i);
+    radioBtn.setAttribute("onclick", "displayDetails(" + i + ")");
+    // radioBtn.onclick = displayDetails(i);
     var radioLbl = document.createElement("LABEL");
     radioLbl.innerHTML = hotel.rooms[i].name;
     document.getElementById("radialSection").appendChild(radioBtn);
     document.getElementById("radialSection").appendChild(radioLbl);
+console.log("Stop");
+    
 }
 
+document.getElementById("reservationForm").onsubmit = function(event) {
+    event.preventDefault();
+    if(!document.getElementById("confirmation").checked) {
+        alert("Please agree to terms motherfucker");
+        return;
+    }
+}
